1399c332420eb7f8d5549eb3babeee82ee4f9f7b
voyage27 <476445136@qq.com>
2023-03-26 21:35:25 +0800

    sync
    sync

--- src/main/java/com/mores/DaMeng.java
+++ src/main/java/com/mores/DaMeng.java
@@ -6,13 +6,13 @@ import snw.jkook.plugin.BasePlugin;
 public class DaMeng extends BasePlugin {
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         getLogger().info("大萌BOT已成功加载");
 
         new JKookCommand("你好")
-                .executesUser((sender,arguments,message)->{
+                .executesUser((sender, arguments, message) -> {
                     String senderName = sender.getName();
-                    message.reply(senderName+"你好，请问我有什么可以帮助你的？");
+                    message.reply(senderName + "你好，请问我有什么可以帮助你的？");
                 })
                 .register(this);
     }
