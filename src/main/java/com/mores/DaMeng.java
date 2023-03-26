514f35fb1cbf117749f4efda1ca2f242967be313
Do0oMores <ws2004421@outlook.com>
2023-03-26 17:09:40 +0800

    Initial commit

---
+++ src/main/java/com/mores/DaMeng.java
@@ -0,0 +1,11 @@
+package com.mores;
+
+import snw.jkook.plugin.BasePlugin;
+
+public class DaMeng extends BasePlugin {
+
+    @Override
+    public void onEnable(){
+        getLogger().info("大萌BOT已成功加载");
+    }
+}
