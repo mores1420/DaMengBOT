9248a1ec22c7c4dbb00c2a16c41b72014e3faced
Do0oMores <ws2004421@outlook.com>
2023-03-26 21:10:47 +0800

    你好命令

--- src/main/java/com/mores/DaMeng.java
+++ src/main/java/com/mores/DaMeng.java
@@ -1,5 +1,6 @@
 package com.mores;
 
+import snw.jkook.command.JKookCommand;
 import snw.jkook.plugin.BasePlugin;
 
 public class DaMeng extends BasePlugin {
@@ -7,5 +8,12 @@ public class DaMeng extends BasePlugin {
     @Override
     public void onEnable(){
         getLogger().info("大萌BOT已成功加载");
+
+        new JKookCommand("你好")
+                .executesUser((sender,arguments,message)->{
+                    String senderName = sender.getName();
+                    message.reply(senderName+"你好，请问我有什么可以帮助你的？");
+                })
+                .register(this);
     }
 }
